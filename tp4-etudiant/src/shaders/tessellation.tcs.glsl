f537cad90a493d3b6e59a5c902886d7206ec84bd
felix-dowl <122920539+felix-dowl@users.noreply.github.com>
2024-12-08 20:33:16 -0500

    attempt to fix

--- tp4-etudiant/src/shaders/tessellation.tcs.glsl
+++ tp4-etudiant/src/shaders/tessellation.tcs.glsl
@@ -18,24 +18,19 @@ void main()
     vec3 patchCenterDistance = (gl_in[0].gl_Position.xyz + gl_in[1].gl_Position.xyz + gl_in[2].gl_Position.xyz + gl_in[3].gl_Position.xyz)/4.0;
     vec3 vectorDistance = gl_in[gl_InvocationID].gl_Position.xyz - patchCenterDistance;
 
-    float distance = sqrt(pow(vectorDistance.x, 2) + pow(vectorDistance.y, 2) + pow(vectorDistance.z, 2));
-    float tessFactor = mix(MIN_TESS, MAX_TESS, clamp((distance - MIN_DIST)/(MAX_DIST - MIN_DIST), 0.0, 1.0));
+    float distance = length(gl_in[gl_InvocationID].gl_Position.xyz);
+    float tessFactor = mix(MAX_TESS, MIN_TESS, clamp((distance - MIN_DIST)/(MAX_DIST - MIN_DIST), 0.0, 1.0));
     
     gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 
     if(gl_InvocationID == 0)
     {
-        gl_TessLevelOuter[0] = distance; // Temporarily output raw distance
-        gl_TessLevelOuter[1] = distance;
-        gl_TessLevelOuter[2] = distance;
-        gl_TessLevelOuter[3] = distance;
-        gl_TessLevelInner[0] = distance;
-        gl_TessLevelInner[1] = distance;
+        gl_TessLevelOuter[0] = tessFactor;
+        gl_TessLevelOuter[1] = tessFactor;
+        gl_TessLevelOuter[2] = tessFactor;
+        gl_TessLevelOuter[3] = tessFactor;
+
+        gl_TessLevelInner[0] = tessFactor;
+        gl_TessLevelInner[1] = tessFactor;
     }
-
-    // gl_in[0].gl_Position; // (0,0)
-    // gl_in[1].gl_Position; // (1,0)
-    // gl_in[2].gl_Position; // (1,1)
-    // gl_in[3].gl_Position; // (0,1)
-
 }
